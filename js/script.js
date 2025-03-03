4cbfb9a8b9cc5e6d94b926c5f8d3cb253f3eaaa3
Jianuo <Jianuojin@gmail.com>
2025-03-04 00:53:15 +0100

    Cambios en .Js adivina número

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ document.getElementById("btnCambio").addEventListener("click", function() {
     const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
     document.body.style.backgroundColor = colorAleatorio;
 });
+
+const numeroSecreto = Math.floor(Math.random()* 10) + 1;
+
+function verficarNumero() {
+    let numeroUsuario = parseInt (document.getElementById("inputNumero").value);
+    let mensaje = "";
+
+    if (numeroUsuario === numeroSecreto) {
+        mensaje = "¡Felicidades! Adivinaste el número secreto.";
+    } else {
+        mensaje = "Número incorrecto. Intenta de nuevo.";
+    }
+
+    document.getElementById("resultado").textContent = mensaje;
+}
