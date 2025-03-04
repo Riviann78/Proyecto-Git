6885e2e6bfd0d04af6f4be28f040d828a6fbc349
Jianuo <Jianuojin@gmail.com>
2025-03-04 19:43:58 +0100

    Agrego una calculadora básica

--- js/script.js
+++ js/script.js
@@ -26,3 +26,19 @@ document.getElementById("botonAdivinar").addEventListener("click", function () {
         resultado.style.color = "red";
     }
 });
+
+function agregarValor(valor) {
+    document.getElementById("pantalla").value += valor;
+}
+
+function calcular() {
+    try {
+        document.getElementById("pantalla").value = eval(document.getElementById("pantalla").value);
+    } catch {
+        document.getElementById("pantalla").value = "Error";
+    }
+}
+
+function limpiar() {
+    document.getElementById("pantalla").value = "";
+}
