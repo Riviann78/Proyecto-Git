9c9aea7f0d18f276ebb0acabf6efc7220a631ea0
Jianuo <Jianuojin@gmail.com>
2025-03-04 22:57:46 +0100

    Reloj digital js

--- js/script.js
+++ js/script.js
@@ -27,3 +27,20 @@ document.getElementById("botonAdivinar").addEventListener("click", function () {
     }
 });
 
+function actualizarReloj() {
+    const ahora = new Date();
+    let horas = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
+
+    horas = horas < 10 ? "0" + horas : horas;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    const horaActual = horas + ":" + minutos + ":" + segundos;
+    document.getElementById("reloj").innerText = horaActual;
+}
+
+setInterval(actualizarReloj, 1000);
+
+actualizarReloj();
