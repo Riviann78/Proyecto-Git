58aeaf551062ede35384a6a65ff79b26accbe80c
Jianuo <Jianuojin@gmail.com>
2025-03-04 22:48:21 +0100

    Reloj digital js

--- js/script.js
+++ js/script.js
@@ -27,18 +27,3 @@ document.getElementById("botonAdivinar").addEventListener("click", function () {
     }
 });
 
-function agregarValor(valor) {
-    document.getElementById("pantalla").value += valor;
-}
-
-function calcular() {
-    try {
-        document.getElementById("pantalla").value = eval(document.getElementById("pantalla").value);
-    } catch {
-        document.getElementById("pantalla").value = "Error";
-    }
-}
-
-function limpiar() {
-    document.getElementById("pantalla").value = "";
-}
