ca3f04a3e243fa9e12531aba1efd421ac798266f
Riviann <riviann.anddos@educa.jcyl.es>
2025-03-05 01:03:02 +0100

    Se añade una calculadora

--- js/script.js
+++ js/script.js
@@ -26,3 +26,21 @@ document.getElementById("botonAdivinar").addEventListener("click", function () {
         resultado.style.color = "red";
     }
 });
+
+function actualizarReloj() {
+    const ahora = new Date();
+    let horas = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
+
+    horas = horas < 10 ? "0" + horas : horas;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    const horaActual = horas + ":" + minutos + ":" + segundos;
+    document.getElementById("reloj").innerText = horaActual;
+}
+
+setInterval(actualizarReloj, 1000);
+
+actualizarReloj();
